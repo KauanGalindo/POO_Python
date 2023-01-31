4a7faf6bb90b2a30783d600184a3b73732552f2c
Kauan Galindo <102545719+KauanGalindo@users.noreply.github.com>
2023-01-31 12:05:15 -0300

    Add files via upload

---
+++ Perimetro_e_Area_do_Quadrado.py
@@ -0,0 +1,6 @@
+ladoDoQuadrado = int(input('Digite o valor correspondente ao lado de um quadrado: '))
+
+perimetroQuadrado = ladoDoQuadrado * 4 
+areaQuadrado = ladoDoQuadrado ** 2
+
+print('perímetro:', perimetroQuadrado,'- área:',areaQuadrado)
